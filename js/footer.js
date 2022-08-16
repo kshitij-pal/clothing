d587ec8b47b325bb18d54e6c214319c9147873cf
Kshitij Pal <kshitijpalcse1@gmail.com>
2022-08-16 17:42:02 +0530

    clothing line

---
+++ js/footer.js
@@ -0,0 +1,75 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+    <img src="img/light-logo.png" class="logo" alt="">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+        <ul class="category">
+            <li class="category-title">women</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+    </div>
+
+</div>
+<p class="footer-title">about company</p>
+<p class="info">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Praesentium perspiciatis ipsam veritatis fugiat ducimus quas maiores temporibus voluptatem
+    nisi quidem quod est rerum, eaque necessitatibus sed deserunt iure. Maiores, ullam? Lorem ipsum,
+    dolor sit amet consectetur adipisicing elit. Omnis eaque placeat tempora numquam, odio eius quia
+    expedita deserunt assumenda excepturi animi voluptas! Numquam dicta animi, totam, ex officiis
+    natus maiores tenetur quod similique excepturi cumque veritatis unde vero voluptatibus accusamus
+    vitae soluta aut, laudantium ratione quidem! Assumenda labore velit quaerat deserunt voluptatum
+    sequi sint maiores quibusdam aspernatur ipsum odit qui aliquid, laboriosam ut magnam quis, repellat
+    consequuntur! Sed, mollitia laudantium? Aut ad ut officia cupiditate dolore incidunt autem dignissimos
+    ratione non, numquam provident deserunt? Officiis similique recusandae dolore, dolorum, maiores quidem
+    voluptate enim, amet obcaecati ex ullam. Quis cum reprehenderit beatae provident earum, et soluta vitae
+    quibusdam rem aut debitis ullam minima tenetur nulla labore nesciunt, numquam sit voluptas deserunt
+    odit,error magni alias nemo? Distinctio totam nam itaque corporis ea porro quisquam neque at sapiente
+    similiq ullam, dignissimos minima quia tempora excepturi, dolorem iste asperiores est deserunt illum nemo
+    repellat rem. Rem aperiam cum quod odio qui dolor veniam sed, voluptatum porro id omnis accusantium eligendi
+    obcaecati nemo necessitatibus asperiores, accusamus quo vitae atque quis inventore temporibus soluta? Est
+    repellat reiciendis aliquam dolorum accusamus! Eum ab tempora exercitationem unde praesentium beatae
+    quisquam, magnam dignissimos iste quasi, cumque, atque sit!</p>
+    <p class="info"> support emails- help@clothing.com, customersupport@clothing.com</p>
+    <p class="info"> telephone - 180 00 00 001, 180 00 00 002</p>
+    <div class="footer-social-container">
+        <div>
+            <a href="#" class="social-link">terms and services</a>
+            <a href="#" class="social-link">privacy pages</a>
+        </div>
+        <div>
+            <a href="#" class="social-link">instagram</a>
+            <a href="#" class="social-link">facebook</a>
+            <a href="#" class="social-link">twitter</a>
+            <a href="#" class="social-link">reddit</a>
+        </div>
+    </div>
+
+    <p class="footer-credit">Clothing, Best apparels online store</p>
+    `;
+}
+
+createFooter();
